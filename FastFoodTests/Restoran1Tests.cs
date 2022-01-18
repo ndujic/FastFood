681f0d8739737ea4df32ffd2e91c4da074ccf022
Duje Junakovic <djunakovi@pmfst.hr>
2022-01-18 16:59:20 +0100

    zavrsna verzija

---
+++ FastFoodTests/Restoran1Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FastFood;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Tests
+{
+    [TestClass()]
+    public class Restoran1Tests
+    {
+        [TestMethod()]
+        public void Restoran1Test()
+        {
+            int ocekivani = 0;
+            Restoran1 rest = new Restoran1();
+
+
+            Assert.AreEqual(ocekivani, rest.ukupnaCijena);
+        }
+    }
+}
